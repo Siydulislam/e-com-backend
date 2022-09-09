168541cefd8d8c3d840d7b2dfaa4cc6db2ed827a
Siydulislam <siydulislam333@gmail.com>
2022-09-09 08:21:12 +0600

    product model and router

---
+++ controllers/productControllers.js
@@ -0,0 +1,18 @@
+const _ = require('lodash');
+const { } = require('../models/product');
+
+module.exports.createProduct = async (req, res) => {
+
+}
+
+module.exports.getProducts = async (req, res) => {
+
+}
+
+module.exports.getProductById = async (req, res) => {
+
+}
+
+module.exports.updateProductById = async (req, res) => {
+
+}
