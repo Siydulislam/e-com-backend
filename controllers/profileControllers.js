0df25ee18ea1b764f202aca4d66bf5502890acd4
Siydulislam <siydulislam333@gmail.com>
2022-09-13 08:11:57 +0600

    profile model and router

---
+++ controllers/profileControllers.js
@@ -0,0 +1,10 @@
+const _ = require('../models/profile');
+const { Profile } = require('../models/profile');
+
+module.exports.getProfile = async (req, res) => {
+
+}
+
+module.exports.setProfile = async (req, res) => {
+
+}
